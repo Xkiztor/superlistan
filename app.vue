51bae2292613d2ac8ade65ef998537b32543d793
Xki <89602681+Xkiztor@users.noreply.github.com>
2022-09-17 13:14:47 +0200

    Test

--- app.vue
+++ app.vue
@@ -12,19 +12,19 @@
 </template>
 
 <script setup>
-useHead({
-  title: 'Super-lista',
-  // or, instead:
-  // titleTemplate: (title) => `My App - ${title}`,
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  charset: 'utf-8',
-  meta: [
-    { name: 'description', content: 'En lista med tusentals växter att beställa från' }
-  ],
-  bodyAttrs: {
-    class: 'test'
-  }
-})
+// useHead({
+//   title: 'Super-lista',
+//   // or, instead:
+//   // titleTemplate: (title) => `My App - ${title}`,
+//   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+//   charset: 'utf-8',
+//   meta: [
+//     { name: 'description', content: 'En lista med tusentals växter att beställa från' }
+//   ],
+//   bodyAttrs: {
+//     class: 'test'
+//   }
+// })
 
 
 </script>
